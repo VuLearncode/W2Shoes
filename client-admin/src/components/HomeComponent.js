b85804f4af9304bbc97acb86025c2fc50bbda8bf
VuLearncode <vulearnit@gmail.com>
2023-11-30 22:01:59 +0700

    upload source

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">SHOES ...</h2>
+        <img src="https://images.pexels.com/photos/267202/pexels-photo-267202.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width="800px" height="600px" alt="" />
+      </div>
+    );
+  }
+}
+export default Home;
